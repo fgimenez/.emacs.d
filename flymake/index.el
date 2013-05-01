6d5cb0049883ac3d1f470cc97698ae4addf9a313
Federico Gimenez Nieto <federico.gimenez@gmail.com>
2013-05-01 13:18:03 +0200

    Initial import

---
+++ flymake/index.el
@@ -0,0 +1,2 @@
+(load "~/.emacs.d/flymake/ruby")
+(load "~/.emacs.d/flymake/php")
---
+++ init.el
@@ -0,0 +1,39 @@
+(global-font-lock-mode 1)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq tab-stop-list (number-sequence 2 200 2))
+(setq indent-line-function 'insert-tab)
+(setq c-basic-offset 2)
+(setq c-basic-indent 2)
+;;(add-hook 'php-mode-hook (lambda () (setq indent-tabs-mode nil)))
+;;(add-hook 'php-mode-hook (lambda () (setq tab-width 2)))
+
+(require 'package)
+(setq package-list '(auto-complete haml-mode magit php-mode rsense jump inflections findr ruby-mode web-mode yaml-mode flymake-easy flymake-jshint flymake-jslint flymake-php flymake-phpcs flymake-ruby))
+(setq package-archives '(("elpa" . "http://tromey.com/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rhtml$" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
+(load "~/.emacs.d/flymake/index")
