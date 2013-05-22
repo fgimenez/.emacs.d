9a3bbc8ce6861a10bbc4a82bd54838898a629769
fgimenez <federico.gimenez@hola-internet.com>
2013-05-22 10:06:21 +0200

    rake files in ruby mode

--- init.el
+++ init.el
@@ -25,6 +25,7 @@
     (package-install package)))
 
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rhtml$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
