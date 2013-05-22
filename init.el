e050b7d18909eef87fb892b03b63f67e06b641c2
Federico Gimenez Nieto <federico.gimenez@gmail.com>
2013-05-22 09:04:33 +0200

    Guard and Gemfile in ruby mode

--- init.el
+++ init.el
@@ -25,6 +25,8 @@
     (package-install package)))
 
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rhtml$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
