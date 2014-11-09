14576a9ead9773ca528ae438e8030d1e95046ac3
Federico Gimenez <fgimenez@coit.es>
2014-11-09 09:48:05 +0100

    added elpa files; restricted to go-only and markdown modules

---
+++ elpa/feature-mode-20141018.646/feature-mode-autoloads.el
@@ -0,0 +1,31 @@
+;;; feature-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (feature-mode) "feature-mode" "feature-mode.el"
+;;;;;;  (21599 10728 752437 584000))
+;;; Generated autoloads from feature-mode.el
+
+(autoload 'feature-mode "feature-mode" "\
+Major mode for editing plain text stories
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.feature\\'" . feature-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("feature-mode-pkg.el") (21599 10728 910902
+;;;;;;  689000))
+
+;;;***
+
+(provide 'feature-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; feature-mode-autoloads.el ends here
---
+++ elpa/findr-20130824.1207/findr-autoloads.el
@@ -0,0 +1,18 @@
+;;; findr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (21599 10722
+;;;;;;  332334 238000))
+
+;;;***
+
+(provide 'findr-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; findr-autoloads.el ends here
