1e15fd726cb0722daf2468e7e45536b6a4dcd1be
Federico Gimenez <federico@giantswarm.io>
2017-08-31 09:54:35 +0200

    removed clojure and ocaml

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 
 (require 'package)
 (defvar package-list)
-(setq package-list '(auto-complete magit jump inflections findr ruby-mode web-mode yaml-mode flycheck feature-mode markdown-mode json-mode go-mode go-autocomplete jedi dockerfile-mode clojure-mode cider tuareg merlin flycheck-ocaml terraform-mode solidity-mode))
+(setq package-list '(auto-complete magit jump inflections findr ruby-mode web-mode yaml-mode flycheck feature-mode markdown-mode json-mode go-mode go-autocomplete jedi dockerfile-mode terraform-mode solidity-mode))
 (setq package-archives '(("elpa" . "http://tromey.com/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
@@ -41,7 +41,6 @@
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.go" . go-mode))
 (add-to-list 'auto-mode-alist '("Dockerfile$" . dockerfile-mode))
-(add-to-list 'auto-mode-alist '("\\.ml" . tuareg-mode))
 
 (require 'auto-complete)
 (require 'go-autocomplete)
@@ -73,48 +72,6 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (column-number-mode)
-
-;; OCaml code
-(add-hook
- 'tuareg-mode-hook
- (lambda ()
-   ;; Add opam emacs directory to the load-path
-   (setq opam-share
-         (substring
-          (shell-command-to-string "opam config var share 2> /dev/null")
-          0 -1))
-   (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
-   ;; Load merlin-mode
-   (require 'merlin)
-   ;; Start merlin on ocaml files
-   (add-hook 'tuareg-mode-hook 'merlin-mode t)
-   (add-hook 'caml-mode-hook 'merlin-mode t)
-   ;; Enable auto-complete
-   (setq merlin-use-auto-complete-mode 'easy)
-   ;; Use opam switch to lookup ocamlmerlin binary
-   (setq merlin-command 'opam)
-   (company-mode)
-   (require 'ocp-indent)
-   (autoload 'utop-minor-mode "utop" "Minor mode for utop" t)
-   (autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
-   (autoload 'merlin-mode "merlin" "Merlin mode" t)
-   (utop-minor-mode)
-   (company-quickhelp-mode)
-   ;; Important to note that setq-local is a macro and it needs to be
-   ;; separate calls, not like setq
-   (setq-local merlin-completion-with-doc t)
-   (setq-local indent-tabs-mode nil)
-   (setq-local show-trailing-whitespace t)
-   (setq-local indent-line-function 'ocp-indent-line)
-   (setq-local indent-region-function 'ocp-indent-region)
-   (if (equal system-type 'darwin)
-       (load-file "/Users/Edgar/.opam/working/share/emacs/site-lisp/ocp-indent.el")
-     (load-file "/home/gar/.opam/working/share/emacs/site-lisp/ocp-indent.el"))
-   (merlin-mode)))
-
-(add-hook 'utop-mode-hook (lambda ()
-                            (set-process-query-on-exit-flag
-                                  (get-process "utop") nil)))
 
 (provide 'init)
 ;;; init.el ends here
