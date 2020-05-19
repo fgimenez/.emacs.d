3d5959d0410982714abd5d91060670877533a583
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-19 10:27:15 +0200

    add previous keystroke

--- init.el
+++ init.el
@@ -131,3 +131,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(global-set-key "\C-xp" (lambda ()
+                          (interactive)
+                          (other-window -1)))
