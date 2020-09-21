0225af254308ef928f8453842d2a682a82044fe3
Federico Gimenez <federico.gimenez@gmail.com>
2020-09-21 11:03:32 +0200

    rust package

---
+++ lisp/rust.el
@@ -0,0 +1,11 @@
+;;; package --- Summary
+;;; Code:
+;;; Commentary:
+(use-package rustic)
+(add-hook 'before-save-hook 'rustic-format-buffer)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+(setq company-tooltip-align-annotations t)
+
+(provide 'rust)
+;;; rust.el ends here
