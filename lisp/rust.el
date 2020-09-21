4e7095e222909fcea0c380fb239840f49faa52d9
Federico Gimenez <federico.gimenez@gmail.com>
2020-09-21 17:46:22 +0200

    removed key bindings (break rustic)

--- lisp/rust.el
+++ lisp/rust.el
@@ -6,9 +6,7 @@
   (add-hook 'before-save-hook #'rustic-format-buffer)
   (add-hook 'racer-mode-hook #'eldoc-mode)
   (add-hook 'racer-mode-hook #'company-mode)
-  (setq company-tooltip-align-annotations t)
-  :bind (("C-c C-c" . rustic-compile)
-         ("C-c C-r" . rustic-cargo-run)))
+  (setq company-tooltip-align-annotations t))
 
 (provide 'rust)
 ;;; rust.el ends here
