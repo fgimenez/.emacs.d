e1c8b30d6793d23e1eadd2978b5940b4928ab0b3
Federico Gimenez <fgimenez@redhat.com>
2020-12-29 10:39:24 +0100

    updated golang lsp

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
 (load-user-file "~/.emacs.d/lisp/basic.el")
 (load-user-file "~/.emacs.d/lisp/packages.el")
 (load-user-file "~/.emacs.d/lisp/file-extensions.el")
-(load-user-file "~/.emacs.d/lisp/lsp.el")
+;(load-user-file "~/.emacs.d/lisp/lsp.el")
 ;(load-user-file "~/.emacs.d/lisp/rust.el")
 (load-user-file "~/.emacs.d/lisp/golang.el")
 (load-user-file "~/.emacs.d/lisp/typescript.el")
--- lisp/golang.el
+++ lisp/golang.el
@@ -1,45 +1,34 @@
 ;;; package --- Summary
 ;;; Code:
 ;;; Commentary:
-(setq lsp-gopls-staticcheck t)
-(setq lsp-gopls-complete-unimported t)
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred))
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
 (defun lsp-go-install-save-hooks ()
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 
-(defun custom-go-mode ()
-  (display-line-numbers-mode 1))
-
-(defun my-compilation-hook ()
-  (when (not (get-buffer-window "*compilation*"))
-    (save-selected-window
-      (save-excursion
-        (let* ((w (split-window-vertically))
-               (h (window-height w)))
-          (select-window w)
-          (switch-to-buffer "*compilation*")
-          (shrink-window (- h compilation-window-height)))))))
-
-;; TODO(abrahms): maybe gopath isn't setup??
-(use-package golint
-  :requires go-mode
-  :hook go-mode
-  :init
-  (add-to-list 'load-path (expand-file-name "~/src/golang.org/x/lint/misc/emacs")))
-
-(use-package go-mode
-  :defer t
+;; Optional - provides fancier overlays.
+(use-package lsp-ui
   :ensure t
-  :mode ("\\.go\\'" . go-mode)
-  :init
-  (setq compile-command "echo Building... && go build -v && echo Testing... && go test -v && echo Linter... && golint")
-  (setq compilation-read-command nil)
-  (setq compilation-window-height 14)
-  (add-hook 'go-mode-hook 'custom-go-mode)
-  (add-hook 'compilation-mode-hook 'my-compilation-hook)
-  :bind (("M-," . compile)
-         ("M-." . godef-jump)))
+  :commands lsp-ui-mode)
 
-(provide 'golang)
+;; Company mode is a standard completion package that works well with lsp-mode.
+(use-package company
+  :ensure t
+  :config
+  ;; Optionally enable completion-as-you-type behavior.
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1))
+
+;; Optional - provides snippet support.
+(use-package yasnippet
+  :ensure t
+  :commands yas-minor-mode
+  :hook (go-mode . yas-minor-mode))
 ;;; golang.el ends here
