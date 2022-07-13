2cdae88cec4f187bbd0969fe7423a5534f332f7a
Federico Gimenez <federico.gimenez@gmail.com>
2022-07-13 14:04:43 +0200

    updated lsp and rust :)

--- lisp/lsp.el
+++ lisp/lsp.el
@@ -2,33 +2,33 @@
 ;;; Code:
 ;;; Commentary:
 (use-package lsp-mode
-             :ensure t
-             :commands (lsp lsp-deferred)
-             :hook (go-mode . lsp-deferred))
+  :ensure
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  (lsp-prefer-capf t)
+  ;; enable / disable the hints as you prefer:
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode 'go-mode))
+
 (setq lsp-eldoc-render-all t)
 (use-package lsp-ui
-             :ensure t
-             :commands lsp-ui-mode)
-
-;;Company mode is a standard completion package that works well with lsp-mode.
-;;company-lsp integrates company mode completion with lsp-mode.
-;;completion-at-point also works out of the box but doesn't support snippets.
-(use-package company
-             :ensure t
-             :config
-             (setq company-idle-delay 0)
-             (setq company-minimum-prefix-length 1))
-
-(use-package company-lsp
-             :ensure t
-             :commands company-lsp)
-
-;;Optional - provides snippet support.
-
-(use-package yasnippet
-             :ensure t
-             :commands yas-minor-mode
-             :hook (go-mode . yas-minor-mode))
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
 
 (setq lsp-ui-peek-enable t
       lsp-ui-sideline-enable t
