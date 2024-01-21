7839c937d706f680c877c36ff6038eb2173d83c7
Federico Gimenez <federico.gimenez@gmail.com>
2024-01-21 20:18:53 +0100

    removed unused file

--- lisp/sway.el
+++
@@ -1,8 +0,0 @@
-;;; package --- Summary
-;;; Code:
-;;; Commentary:
-(add-to-list 'load-path "/home/fgimenez/workspace/fgimenez/sway-mode")
-(autoload 'sway-mode "sway-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.sw\\'" . sway-mode))
-(provide 'sway)
-;;; sway.el ends here
