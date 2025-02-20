35bbb13d85823e1a6f9917a04147486c5ed376b3
Federico Gimenez <federico.gimenez@gmail.com>
2025-02-20 10:17:55 +0100

    more async

--- lisp/async.el
+++ lisp/async.el
@@ -4,4 +4,8 @@
 (use-package async
   :ensure t
   :init (async-bytecomp-package-mode 1))
+
+(setq native-comp-async-report-warnings-errors nil)
+(setq native-comp-deferred-compilation t)
+
 ;;; async.el ends here
